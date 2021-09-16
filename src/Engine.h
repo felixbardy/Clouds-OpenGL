f15003481484195994197d681914271e7e2578f3
Duskel <Bastien.ruivo@gmail.com>
2021-09-16 12:53:31 +0200

    doc: documentation de l'engine

--- src/Engine.h
+++ src/Engine.h
@@ -11,16 +11,37 @@
 
 using namespace glm;
 
-
+/** @class Engine
+ * @brief
+ * 
+ * Gère le rendu global
+ * 
+ */
 class Engine
 {
     public:
 
     World * zaWarudo;
+    /** @brief Initialise les variables
+     * @param vertexPath chemin vers le shader de vertex
+     * @param fragmentPath chemin vers le shader de fragment
+     */
     void init(std::string vertexPath, std::string fragmentPath, uint w = 500, uint h = 500);
+
+    /** @brief Demarre le rendu      */
     void run();
+
+    /** @brief fermer le rendu   */
     void close();
+    /** @brief Change la couleur d'arrière plan
+     * @param r valeur entre 0 et 1 de rouge
+     * @param g valeur entre 0 et 1 de vert
+     * @param b valeur entre 0 et 1 de bleue
+     * @param a valeur entre 0 et 1 de transparence
+     */
     void setBackgroundColor(float r, float g, float b, float a);
+
+    /** @brief Accesseur adresse memoire shader */
     Shader* getShader();
     float lastTime = 0;
     Textures textureAltas;
@@ -31,6 +52,10 @@ class Engine
     Shader shader;
     int inputPrevent;
     bool isWireframe = false;
+
+    /** @brief Gère les inputs
+     * @param Cam adresse memoire de la camera
+     */
     void keyboardHandler(Camera * Cam);
 
     float r, g, b, a;
